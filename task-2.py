feae31753b938d3d4dbc98fab52b50ba8570b1af
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-02-09 15:55:28 +0000

    finished task 2

---
+++ task-2.py
@@ -0,0 +1,9 @@
+def perimeter_of_rectangle(l, w):
+    return (l * 2) + (w * 2)
+
+print("Please enter the length of the rectangle:")
+length = float(input())
+print("Please enter the width of the rectangle:")
+width = float(input())
+
+print("The perimeter of the rectangle is: " + str(perimeter_of_rectangle(length, width)))
